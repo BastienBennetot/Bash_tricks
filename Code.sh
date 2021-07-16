7b05ab4acb412ede076f97ebbfe845c327890bf3
Bastien Bennetot <86835441+BastienBennetot@users.noreply.github.com>
2021-07-16 22:33:56 +0200

    Create Code.sh

---
+++ Code.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#merge multiple csv file
+#OFS : separator of your file, below a comma
+
+#With Header
+  # Adding a column with filenames
+    awk -v OFS=',' 'FNR==1 && NR!=1{next;}{print $0, FILENAME}' *.csv >merged_result.csv
+  # Not adding a filename column
+    awk '(NR ==1) || (FNR>1)' *.csv >merged.csv
+
+#Without Header
+  # Adding a column with filenames
+    awk -v OFS=',' '{print FILENAME, $0}' *.csv >merged.csv
+  # Not adding a filename column
+    cat *.csv >merged.csv
+
